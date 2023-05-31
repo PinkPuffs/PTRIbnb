13298ecfbb27c2727498c361c1dff418deb52efc
IuliiaSam <iuliia.samokhvalova@gmail.com>
2023-05-30 21:07:14 -0400

    tailwind configuration

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import '../tailwind.css';
 
 function App() {
-  return <h1>PTRI BnB</h1>;
+  return (
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <h1 className="text-2xl test-class">PTRI BnB</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1 className="text-2xl text-blue-500">Home page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
