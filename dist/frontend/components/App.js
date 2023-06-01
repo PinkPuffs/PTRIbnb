0f476d7b63fe7c18f7ea74f08e088e34dc470a5a
zhiyi15 <zhiyihuang1994@gmail.com>
2023-05-31 18:51:17 -0700

    deleted ./dist

--- dist/frontend/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function App() {
-    return <h1>PTRI BnB</h1>;
-}
-export default App;
--- dist/frontend/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-createRoot(document.querySelector('#app')).render(<React.StrictMode>
-        <App />
-  </React.StrictMode>);
-//ask group about what happens when you use prettier here (turns into one line)
-// You made it prettier?
